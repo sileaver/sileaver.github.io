7fba92c62e798d1ab82b873c5e6dd6596baf4ef9
sileaver <541127889@qq.com>
2022-04-24 21:33:41 +0800

    Whatever

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\a\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\a\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\a\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
+  require('C:\\Users\\a\\my-website\\src\\css\\custom.css'),
+];
